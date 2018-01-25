c9be7cbf2d2674e88c5247334d3a6a3912f4cd9d
Ondrej Tomco <ondrej.tomco@akademiasovy.sk>
2018-01-25 13:00:56 +0100

    temp rounding added

--- script.js
+++ script.js
@@ -7,48 +7,50 @@ $("#submitBtn").click(function(){
     if(city.length > 1){
         var urlLink = 'http://api.openweathermap.org/data/2.5/weather?q=';
         urlLink = urlLink + city;
-    }
 
-    if(country.length == 2){
-        urlLink = urlLink + ',' + country;
-        
-    }
-
-    urlLink = urlLink + '&APPID=a84fd6bdde590e3024412e55a99152c0';
-    console.log(urlLink);
+        urlLink = urlLink + '&APPID=a84fd6bdde590e3024412e55a99152c0';
+        console.log(urlLink);
 
         $.ajax({
             url:urlLink,
             data: { format: 'json'},
             error: function(){
-                console.log('error');
+               $("#error").html('City not Found');
             },
             dataType: "json",
             success : createTable,
             type: 'GET'
 
         });
+    }
 
+    // if(country.length == 2){
+    //     urlLink = urlLink + ',' + country;
+        
+    // }
+
+    
 });
 
 function createTable(data){
+   
 
     $("#weatherContainer").empty();
 
     var table = $('<table/>');
-    table.addClass('table table-bordered');
+    table.addClass('table table-bordered table-striped');
     $("#weatherContainer").append(table);
     table.append("<tr class='col-md-12 headRow text-center'><th colspan ='2'>Current weather in "+data.name+"</th></tr>");
     
 
-    table.append(getTableRow('Temperature: ', Math.round(data.main.temp-273.15) + '°','Humidity: ',data.main.humidity+'%' ));
+    table.append(getTableRow('Temperature: ',parseFloat(data.main.temp-273.15).toFixed(1) + '°','Humidity: ',data.main.humidity+'%' ));
     table.append(getTableRow('Description: ', data.weather[0].description,'Pressure: ',data.main.pressure + ' hPa' ));
 
     if($('#showDetails').is(":checked")){
 
         table.append(getTableRow('Sunrise: ', convertTime(data.sys.sunrise),'Sunset: ',convertTime(data.sys.sunset) ));
         table.append(getTableRow('Wind speed: ', data.wind.speed + 'm/s','Visibility: ',data.visibility ));
-        table.append(getTableRow('Min temperature: ', Math.round(data.main.temp_min-273.15)+ '°','Max temperature: ',Math.round(data.main.temp_max-273.15)+ '°' ));
+        table.append(getTableRow('Min temperature: ', parseFloat(data.main.temp_min-273.15).toFixed(1)+ '°','Max temperature: ',parseFloat(data.main.temp_max-273.15).toFixed(1)+ '°' ));
 
     }
 
@@ -63,6 +65,7 @@ function getTableRow(name1,data1,name2,data2){
     row.append(column2);
     column2.html(name2 + data2);
     return row;
+    
 }
 
 function convertTime(data){
